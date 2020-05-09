b9a9217f4f96b63f7d45c71a74653bdf32a71dac
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-08 21:38:11 -0400

    GET and POST done

--- main.js
+++ main.js
@@ -11,5 +11,36 @@ notesForm.addEventListener ('submit' , function (event) {
 })
 
 function createNewNote (noteText) {
-    
+    fetch ("http://localhost:3000/notes" , {
+        method: 'POST' , 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ item: noteText, done: false, created: moment().format() })
+
+    })
+    .then (response => response.json ()) 
+    .then (data => console.log (data))
 }
+
+function renderNotes () {
+    fetch ("http://localhost:3000/notes" , {
+        method: 'GET'
+})
+    .then(response => response.json())
+    .then(function (data) {
+    let list = document.createElement ("ul")
+    for (let item of data) {
+        let listItem = document.createElement ("li")
+        listItem.dataset.id = item.id
+        listItem.innerText = item.item 
+        list.appendChild (listItem)
+    }
+    let noteList = document.querySelector(".notelist")
+    noteList.appendChild (list)
+
+    }) 
+}
+
+renderNotes ()
+
+
+        
